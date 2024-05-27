638b5f42d2bd134e50d368fdf9a3930eadbaaccc
epili50 <epili50@gmail.com>
2024-05-27 21:09:01 +0200

    hecho el del DOM

--- dom/app.js
+++ dom/app.js
@@ -1,1 +1,18 @@
 // ¿En tu páis de origen es normal dejar propina?
+
+
+const calculate = document.querySelector('#calculate');
+
+calculate.addEventListener('click', function(){
+    let billAmount = document.querySelector('#bill-amount').value;
+    let porcentageTip = document.querySelector('#percentage-tip').value;
+    console.log(+billAmount);
+    
+    const tipAmount = (+porcentageTip / 100) * +billAmount;
+    console.log("🚀 ~ file: app.js:11 ~ calculate.addEventListener ~ tipAmount:", tipAmount)
+    const total = +billAmount + tipAmount
+    console.log("🚀 ~ file: app.js:13 ~ calculate.addEventListener ~ total:", total)
+
+    document.querySelector('#tip-amount').value = tipAmount.toFixed(2);
+    document.querySelector('#total').value = total.toFixed(2);
+})
