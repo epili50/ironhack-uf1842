436c54ff9a9f5240ab0aa3db0164d407c09e9689
epili50 <epili50@gmail.com>
2024-05-27 19:28:04 +0200

    Hecho Algoritms 1 y 2

--- algoritmia/2-match-uefa-euro.js
+++ algoritmia/2-match-uefa-euro.js
@@ -8,6 +8,13 @@
  * @returns {string} Resultado del partido, siguiendo el formato indicado en los comentarios, según si ha ganado el primer equipo, el segundo, o ha habido empate.   
 */
 function uefaEuro2016(teams, scores) {
+    if(scores[0] > scores[1]){
+        return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
+    }else if (scores[0] < scores[1]){
+        return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+    }else{
+        return  `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+    }
 
 }
 
